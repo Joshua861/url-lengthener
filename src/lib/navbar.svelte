3f7822ba9a80bc5a59b2b410c2d5df110f233381
Joshua861 <joshrl@proton.me>
2024-04-17 21:01:19 +0100

    .

--- src/lib/navbar.svelte
+++ src/lib/navbar.svelte
@@ -16,5 +16,4 @@
 <div class="prose prose-2xl mx-auto mt-3 flex px-3 dark:prose-invert">
 	<a href="/" class="flex-1 font-mono no-underline">url lengthener</a>
 	<Button variant="outline" size="icon" on:click={toggleTheme}><SunMoon /></Button>
-	<Button href="/about" class="ml-3 no-underline" variant="outline">about</Button>
 </div>
